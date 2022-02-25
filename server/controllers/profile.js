ae117652200915e2afb0bcda0990b5e7d992bb6d
jogeshgupta963 <jogeshgupta963@gmail.com>
2022-02-25 14:36:41 +0530

    added skill component in arrays

--- server/controllers/profile.js
+++ server/controllers/profile.js
@@ -9,11 +9,13 @@ const jwt = require('jsonwebtoken')
 async function postProfile(req, res) {
     let data = req.body;
     let isExist = await profileModel.findOne({ user: data.user })
-
+    // data.skills = data.skills[0].split(',').map(skills => skills.trim())
+    let skills = data.skills.split(',')
+    data.skills = skills;
     let profile = new profileModel(data);
     if (!isExist) {
         profile.save();
-        return res.json(profile)
+        return res.json("saved")
     }
     // let user = await profileModel.findByIdAndUpdate({ user: data.user }, data)
     let user = await profileModel.findOneAndUpdate({ user: data.user }, data)
